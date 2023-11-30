608a263ff223574bb8743c5d5074cc793079e8f6
Faizan Khan <114975001+gfaizank@users.noreply.github.com>
2023-11-30 14:38:34 +0530

    Update Form.jsx

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -9,17 +9,19 @@ import {
   faEye,
   faImage,
   faFill,
-  faFont,
-  faPencilAlt, 
+  faPencilAlt,
+  faSort,
+  faChevronDown,
+  faSmileBeam,
   faToggleOn,
-   faToggleOff,
-  faSort, faChevronDown, faSmileBeam
+  faToggleOff,
 } from '@fortawesome/free-solid-svg-icons';
 
-const EventForm = () => {
+const EventForm = ({ addEvent }) => {
   const [eventName, setEventName] = useState('');
   const [startDate, setStartDate] = useState('');
   const [startTime, setStartTime] = useState('');
+
   const [endDate, setEndDate] = useState('');
   const [endTime, setEndTime] = useState('');
   const [location, setLocation] = useState('');
@@ -30,11 +32,35 @@ const EventForm = () => {
   const [color, setColor] = useState('');
   const [typeface, setTypeface] = useState('');
 
+  const [isApproved, setIsApproved] = useState(false);
+
+  const handleToggleApproval = () => {
+    setIsApproved(!isApproved);
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    // Perform action with form data (e.g., submit to server)
-    // This is a basic example; you may want to add validation and error handling
+    
+
+    // Log the collected data
+    const formData = {
+      eventName,
+      startDate,
+      startTime,
+      endDate,
+      endTime,
+      location,
+      approvalRequired,
+      capacity,
+      visibility,
+      theme,
+      color,
+      typeface,
+    };
+    console.log('Collected Form Data:', formData);
+
+    addEvent(formData);
 
     // Clear form fields after submission
     setEventName('');
@@ -50,27 +76,24 @@ const EventForm = () => {
     setColor('');
     setTypeface('');
   };
-  const [isApproved, setIsApproved] = useState(false);
-
-  const handleToggleApproval = () => {
-    setIsApproved(!isApproved);
-  };
 
   const handleImageUpload = (e) => {
     // Handle the image upload logic here
     const selectedFile = e.target.files[0];
     console.log('Selected File:', selectedFile);
+
+    // You can send the selectedFile to your server or process it as needed
   };
 
   return (
     <div className="flex">
       {/* FIRST HALF */}
       <div className="w-1/2 p-6">
-      <h2 className="text-lg font-bold mb-4 flex items-center">
-      <FontAwesomeIcon icon={faSmileBeam} className="text-gray-500 mr-2" />
-        Personal Calendar
-        <FontAwesomeIcon icon={faChevronDown} className="text-gray-500 ml-2" />
-      </h2>
+        <h2 className="text-lg font-bold mb-4 flex items-center">
+          <FontAwesomeIcon icon={faSmileBeam} className="text-gray-500 mr-2" />
+          Personal Calendar
+          <FontAwesomeIcon icon={faChevronDown} className="text-gray-500 ml-2" />
+        </h2>
         <form onSubmit={handleSubmit} className="mb-8">
           {/* Event Name */}
           <label className="mb-4">
@@ -145,61 +168,64 @@ const EventForm = () => {
 
           {/* Tickets */}
           <div className="mb-4 flex items-center justify-between">
-  <div className="flex items-center">
-    <FontAwesomeIcon icon={faTicketAlt} className="text-xl mr-2" />
-    Tickets
-  </div>
-  <div className="flex items-center">
-    <span className="mr-2">Free</span>
-    <FontAwesomeIcon icon={faPencilAlt} className="text-sm" />
-  </div>
-</div>
-
+            <div className="flex items-center">
+              <FontAwesomeIcon icon={faTicketAlt} className="text-xl mr-2" />
+              Tickets
+            </div>
+            <div className="flex items-center">
+              <span className="mr-2">Free</span>
+              <FontAwesomeIcon icon={faPencilAlt} className="text-sm" />
+            </div>
+          </div>
 
           {/* Approval Required */}
           <div className="mb-4 flex items-center justify-between">
-      <div className="flex items-center">
-        <FontAwesomeIcon icon={faCheckCircle} className={`text-xl mr-2 ${isApproved ? 'text-green-500' : ''}`} />
-        Approval Required
-      </div>
-      <div className="flex items-center">
-        {/* Toggle Button */}
-        <button
-          className={`text-blue-500 mr-2 text-xl ${isApproved ? ' text-white' : ''}`}
-          onClick={handleToggleApproval}
-        >
-          {isApproved ? <FontAwesomeIcon icon={faToggleOn} /> : <FontAwesomeIcon icon={faToggleOff} />}
-        </button>
-      </div>
-    </div>
+            <div className="flex items-center">
+              <FontAwesomeIcon
+                icon={faCheckCircle}
+                className={`text-xl mr-2 ${isApproved ? 'text-green-500' : ''}`}
+              />
+              Approval Required
+            </div>
+            <div className="flex items-center">
+              {/* Toggle Button */}
+              <button
+                className={`text-blue-500 mr-2 text-xl ${isApproved ? ' text-white' : ''}`}
+                onClick={handleToggleApproval}
+              >
+                {isApproved ? <FontAwesomeIcon icon={faToggleOn} /> : <FontAwesomeIcon icon={faToggleOff} />}
+              </button>
+            </div>
+          </div>
 
           {/* Capacity */}
           <div className="mb-4 flex items-center justify-between">
-  <div className="flex items-center">
-    <FontAwesomeIcon icon={faUsers} className="text-xl mr-2" />
-    Capacity
-  </div>
-  <div className="flex items-center">
-    <span className="mr-2">Unlimited</span>
-    <FontAwesomeIcon icon={faPencilAlt} className="text-sm" />
-  </div>
-</div>
+            <div className="flex items-center">
+              <FontAwesomeIcon icon={faUsers} className="text-xl mr-2" />
+              Capacity
+            </div>
+            <div className="flex items-center">
+              <span className="mr-2">Unlimited</span>
+              <FontAwesomeIcon icon={faPencilAlt} className="text-sm" />
+            </div>
+          </div>
 
           {/* Visibility */}
           <div className="mb-4 flex items-center justify-between">
-  <div className="flex items-center">
-    <FontAwesomeIcon icon={faEye} className="text-xl mr-2" />
-    Visibility
-  </div>
-  <div className="flex items-center">
-    <span className="mr-2">Public</span>
-    <FontAwesomeIcon icon={faSort} className="" />
-  </div>
-</div>
+            <div className="flex items-center">
+              <FontAwesomeIcon icon={faEye} className="text-xl mr-2" />
+              Visibility
+            </div>
+            <div className="flex items-center">
+              <span className="mr-2">Public</span>
+              <FontAwesomeIcon icon={faSort} className="" />
+            </div>
+          </div>
 
           {/* Create Event Button */}
           <button
             type="submit"
+            onClick={handleSubmit}
             className="bg-blue-500 text-white p-2 rounded"
           >
             Create Event
@@ -211,49 +237,49 @@ const EventForm = () => {
       <div className="w-1/2 p-6">
         {/* Image Tile */}
         <div className="mb-4 relative">
-      {/* Replace the URL with your image source */}
-      <img
-        src="https://via.placeholder.com/200"
-        alt="Event Image"
-        className="w-full h-80 object-cover mb-2 rounded"
-      />
-      {/* Image Upload Icon */}
-      <label htmlFor="imageInput" className="absolute bottom-0 right-0 cursor-pointer p-2">
-        <FontAwesomeIcon icon={faImage} className="text-gray-500 text-xl font-bold hover:text-gray-700" />
-        <input
-          type="file"
-          id="imageInput"
-          accept="image/*"
-          className="hidden"
-          onChange={handleImageUpload}
-        />
-      </label>
-    </div>
+          {/* Replace the URL with your image source */}
+          <img
+            src="https://via.placeholder.com/200"
+            alt="Event Image"
+            className="w-full h-80 object-cover mb-2 rounded"
+          />
+          {/* Image Upload Icon */}
+          <label htmlFor="imageInput" className="absolute bottom-0 right-0 cursor-pointer p-2">
+            <FontAwesomeIcon icon={faImage} className="text-gray-500 text-xl font-bold hover:text-gray-700" />
+            <input
+              type="file"
+              id="imageInput"
+              accept="image/*"
+              className="hidden"
+              onChange={handleImageUpload}
+            />
+          </label>
+        </div>
 
         {/* Theme */}
         <h5 className="mb-2">Theme</h5>
         <div className="mb-4 flex items-center justify-between">
-  <div className="flex items-center">
-    <FontAwesomeIcon icon={faFill} className="text-xl mr-2" />
-    Color
-  </div>
-  <div className="flex items-center">
-    <span className="mr-2">Gray</span>
-    <FontAwesomeIcon icon={faSort} className="text-sm" />
-  </div>
-</div>
- 
+          <div className="flex items-center">
+            <FontAwesomeIcon icon={faFill} className="text-xl mr-2" />
+            Color
+          </div>
+          <div className="flex items-center">
+            <span className="mr-2">Gray</span>
+            <FontAwesomeIcon icon={faSort} className="text-sm" />
+          </div>
+        </div>
+
         {/* Typeface */}
         <div className="mb-4 flex items-center justify-between">
-  <div className="flex items-center">
-    <h3 className='font-bold mr-2'>Ag</h3>
-    Typeface
-  </div>
-  <div className="flex items-center">
-    <span className="mr-2">Default</span>
-    <FontAwesomeIcon icon={faSort} className="text-sm" />
-  </div>
-</div>
+          <div className="flex items-center">
+            <h3 className='font-bold mr-2'>Ag</h3>
+            Typeface
+          </div>
+          <div className="flex items-center">
+            <span className="mr-2">Default</span>
+            <FontAwesomeIcon icon={faSort} className="text-sm" />
+          </div>
+        </div>
       </div>
     </div>
   );
