d2ecd17dcafe3731fb6ac8e76856af06e4f55d66
Faizan Khan <114975001+gfaizank@users.noreply.github.com>
2023-11-30 12:25:08 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,6 +10,30 @@ const App = () => {
     setEvents([...events, newEvent]);
   };
 
+  const mockEvents = [
+    {
+      date: 'Mon, Dec 12',
+      time: '3:00 PM',
+      title: "Builder's Roundtable",
+      image: 'https://via.placeholder.com/50',
+      host: 'Octo ML',
+      type: 'Virtual',
+      status: 'Invited',
+      attendees: '30',
+    },
+    {
+      date: 'Thu, Nov 30',
+      time: '12:50 PM',
+      title: "Completing assignments",
+      image: 'https://via.placeholder.com/50',
+      host: 'FK',
+      type: 'Virtual',
+      status: 'Invited',
+      attendees: '13',
+    }
+    // Add more events as needed
+  ];
+
   return (
     <div className="container mx-auto mt-8">
       {/* <h1 className="text-2xl font-bold mb-4">Event Creation Web Application</h1> */}
@@ -19,7 +43,7 @@ const App = () => {
       <Form addEvent={addEvent} />
 
       {/* Event Listing */}
-      <Listing events={events} />
+      <Listing events={mockEvents} />
       
     </div>
   );
